0418b0ec3e8fefbe95b0d4b2bad0c7d8ecd94663
AT <arstarn@yandex.ru>
2022-05-01 20:52:20 +0300

    Update script.js
    add gravity I guess

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const block = document.getElementById('block');
 const hole = document.getElementById('hole');
+const character = document.getElementById("character");
 
 hole.addEventListener('animationiteration', function() {
     const random = -((Math.random()*300)+150);
     hole.style.top = random +"px";
 });
+setInterval(function(){
+    const characterTop = 
+    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    character.style.top = (characterTop + 3) + "px";
+}, 10);
