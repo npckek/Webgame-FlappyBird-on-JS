53bef4a383a534a4ed64adc75b09bec228822716
AT <arstarn@yandex.ru>
2022-05-04 14:12:20 +0300

    birds can fly
    So i add some feathers and now you can fly (also i edit some mistakes, as usual)

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-const block = document.getElementById('block');
-const hole = document.getElementById('hole');
-const character = document.getElementById("character");
+var block = document.getElementById('block');
+var hole = document.getElementById('hole');
+var character = document.getElementById("character");
+var jumping = 0;
 
 hole.addEventListener('animationiteration', function() {
-    const random = -((Math.random()*300)+150);
+    var random = -((Math.random()*300)+150);
     hole.style.top = random +"px";
 });
 setInterval(function(){
-    const characterTop = 
+    var characterTop = 
     parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    character.style.top = (characterTop + 3) + "px";
+    if(jumping == 0) {
+        character.style.top = (characterTop + 3) + "px";
+    }
 }, 10);
+
+function jump() {
+    jumping = 1;
+    let jumpCount = 0;
+    var jumpInterval = setInterval(function() {
+        var characterTop = 
+        parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        if((characterTop > 6) && (jumpCount < 15)) {
+            character.style.top = (characterTop - 5) + "px";
+        }
+        if(jumpCount > 20) {
+            clearInterval(jumpInterval);
+            jumping = 0;
+            jumpCount = 0;
+        }
+        jumpCount++;
+    }, 10);
+}
