759b4e2ca81d54586f48844f7f1cf86225dfb4ea
AT <arstarn@yandex.ru>
2022-05-04 15:01:57 +0300

    last commit i guess
    add some score and stuff, now you can play( i know about troubles with design, but who cares)

--- script.js
+++ script.js
@@ -2,16 +2,30 @@ var block = document.getElementById('block');
 var hole = document.getElementById('hole');
 var character = document.getElementById("character");
 var jumping = 0;
+var counter = 0;
 
 hole.addEventListener('animationiteration', function() {
     var random = -((Math.random()*300)+150);
     hole.style.top = random +"px";
+    counter++;
 });
 setInterval(function(){
     var characterTop = 
     parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     if(jumping == 0) {
         character.style.top = (characterTop + 3) + "px";
+    }
+    var blockLeft = 
+    parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    var holeTop = 
+    parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+    var characterTop = 
+    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var cTop = -(500 - characterTop);    
+    if((characterTop > 480) || ((blockLeft < 20) && (blockLeft > -500) && ((cTop<holeTop)||(cTop>holeTop + 130)))){
+        alert("Game over, Score "+ counter);
+        character.style.top = 100 + "px"; 
+        counter = 0
     }
 }, 10);
 
